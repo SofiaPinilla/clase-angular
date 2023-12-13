d676d1a8cef10f5b215101ce51f63bbdd8f87bb4
SofiaPinilla <contact@sofiapinilla.com>
2023-12-13 12:19:29 +0100

    first commit

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  contactForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log('form', this.contactForm.value);
+    this.contactForm.reset()
+  }
+
+}
